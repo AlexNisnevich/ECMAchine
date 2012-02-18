57529fa188ff1cfc024ee4a1efacb285dedc12db
Alex Nisnevich <alex.nisnevich@gmail.com>
2012-02-18 00:04:08 -0800

    comparison operators

--- ecmachine.js
+++ ecmachine.js
@@ -51,14 +51,30 @@ function parse(sexp) {
  * Evaluates a parsed S-expression, Lisp-style
  */
 function evaluate(sexp) {
+	var controlFlowStatements = ['if', 'cond'];
+	
 	if (typeof sexp != 'object') { // atom
-		return sexp;
+		switch (sexp) {
+			case '#t':
+				return true;
+			case '#f':
+				return false;
+			default:
+				return sexp;
+		}
 	}
 	
 	var func = sexp[0];
-	var args = [];
-	for (var i = 1; i < sexp.length; i++) {
-		args.push(evaluate(sexp[i]));
+	
+	if (controlFlowStatements.indexOf(func) > -1) {
+		// don't evaluate arguments
+		var args = sexp.slice(1);
+	} else {
+		// evaluate arguments
+		var args = [];
+		for (var i = 1; i < sexp.length; i++) {
+			args.push(evaluate(sexp[i]));
+		}
 	}
 	
 	switch(func) {
@@ -79,7 +95,31 @@ function evaluate(sexp) {
 		case '==':
 		case '!=':
 			return eval(args[0] + func + args[1]);
+			
+		// Logical
+		case 'not':
+			return !(args[0]);
+		case 'and':
+			return eval(args.join('&&'));
+		case 'or':
+			return eval(args.join('||'));
+			
+		// Conditionals
+		case 'if':
+			if (evaluate(args[0])) {
+				return evaluate(args[1]);
+			} else {
+				return evaluate(args[2]);
+			}
+		case 'cond':
+			for (var i = 0; i < args.length; i++) {
+				var condBlock = args[i];
+				if (evaluate(condBlock[0])) {
+					return evaluate(condBlock[1]);
+				}
+			}
 		
+		// Unrecognized method
 		default:
 			console.log('Unrecognized method: ' + func);
 			return 'Error';
